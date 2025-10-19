d14701a56ca344d435f259b1c80f0afaccdfdf4c
Jaxon1044 <jfroderberg@zagmail.gonzaga.edu>
2025-10-18 21:10:21 -0700

    feat(register.html): Added basic form validation

--- register_script.js
+++ register_script.js
@@ -2,6 +2,28 @@
     This script is to be used for the registering of user acocunts.S
 */
 
+let submitUserBtn = document.getElementById("submit-user");
+
+// ==============================================================
+// User Account information validation:
+
+// When the user presses the "Create account" button, check that all entered information is valid:
+
+function validateRegistration() {
+
+    let userName = document.getElementById("username").value;
+
+    // Implement later: Check if the username already exists in the database:
+
+    console.log("Does any of this get displayed?");
+
+    let userPassword = document.getElementById("password").value;
+
+    let userPassConfirm = document.getElementById("passwordConfirm").value;
+    
+    let userEmail = document.getElementById("email").value;
+}
+
 // ==============================================================
 // User Account registration:
 
@@ -27,3 +49,6 @@ function checkLoginStatus() {
         console.log("The user is logged in.");
     }
 }
+
+submitUserBtn.addEventListener( "click", validateRegistration );
+

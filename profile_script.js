c5af2e043ea871121b63ecc75cfd5412cd87904b
Jaxon <j.a.froderberg@proton.me>
2025-10-20 17:44:02 -0700

    feat(profile_script.js): Added javaScript outline for user profile pages.

---
+++ profile_script.js
@@ -0,0 +1,16 @@
+/*
+    This script handles the user profile page, which manages all user information including:
+    - User account info
+    - User preference info
+    - User bookmarked restaurants
+    - User reviews
+*/
+
+// Setup functionality to edit user acocunt info ( username, email ):
+
+// Setup functionality to add / remove user preferences from the preference list:
+
+// Add functionality for removing bookmarked restaurants:
+
+// Add functionality for removing / editing restaurant reviews:
+

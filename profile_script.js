4d8d17cf7b5b5a7a72bc16d509f39859b332a691
Jaxon <j.a.froderberg@proton.me>
2025-10-20 18:19:11 -0700

    feat(profile_script.js): Added event handler function for bookmark removal.

--- profile_script.js
+++ profile_script.js
@@ -6,11 +6,34 @@
     - User reviews
 */
 
+window.addEventListener( "DOMContentLoaded", loadedHandler );
+
+function loadedHandler() {
+
+    // Apply removeBookmark event handler to all bookmarks:
+    let bookmarkRemoveBtns = document.getElementsByClassName("remove-bkmk-btn");
+    for ( button of bookmarkRemoveBtns ) {
+        button.addEventListener( "click", removeBookmark );
+    }
+
+   
+}
+
 // Setup functionality to edit user acocunt info ( username, email ):
 
 // Setup functionality to add / remove user preferences from the preference list:
 
 // Add functionality for removing bookmarked restaurants:
+
+// Remove bookmarked restaurant event handler function:
+function removeBookmark( event ) {
+    // Get the <div> of the clicked bookmark:
+    let parent = event.target.parentNode.parentNode;
+    console.log(`${parent}`);
+    parent.parentNode.removeChild.parent;
+    
+    console.log("Removed bookmark!");
+}
 
 // Add functionality for removing / editing restaurant reviews:
 

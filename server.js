66a2478e466df7a21c79784e5bebde25f7d6ddf9
Jaxon <j.a.froderberg@proton.me>
2025-11-07 16:32:09 -0800

    feat(server.js): Added express method to open homepage

--- server.js
+++ server.js
@@ -6,9 +6,21 @@ const app = express(); // Set application object equal to express obj.
 
 // app.use(express.static('__public/webpages-directory__')); // <--- implement later when project files are better organized.
 
+// ================================================================
+
+
+// app method for launcing initial search page:
+app.get("/search", (req, res) => {
+    console.log("Opening search homepage...");
+    // Open search homepage ( homepage.html )
+    const html = homepage.html;
+    res.send( html );
+});
+
+
+// ====================== Launch Server ===========================
+
 app.listen(8000, () => {
   console.log("Listening on port 8000...");
   // Maybe also get homepage / search screen?
 });
-
-// ================================================================

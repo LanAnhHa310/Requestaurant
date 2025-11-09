32e2507d2bae7cb4f7244353daf96bd9a5749247
Jaxon <j.a.froderberg@proton.me>
2025-11-08 18:43:41 -0800

    feat: Moved webpage files to public directory, current server.js cannot load pages.

--- server.js
+++ server.js
@@ -6,22 +6,29 @@ var path = require('path'); // provides utilities for working with file and dire
 
 const app = express(); // Set application object equal to express obj.
 
-// app.use(express.static('__public/webpages-directory__')); // <--- implement later when project files are better organized.
+//app.use(express.static(path.join(__dirname + '/public') ) ); // <--- implement later when project files are better organized.
+app.use(express.static('public'));
 
 // ================================================================
 
+// Catch-all for when project files are not found:
+app.use((req, res) => {
+  res.status(404);
+  res.send('<h1>ERROR: Resource(s) not found</h1>');
+});
 
 // app method for launcing initial search page:
-app.get('/', (req, res) => {
-    console.log("Opening search homepage...");
-    // Open search homepage ( homepage.html )
-    res.sendFile(__dirname + '/homepage.html');
+app.get('/search', (req, res) => {
+  console.log("Opening search homepage...");
+  // Open search homepage ( homepage.html )
+  //res.sendFile(__dirname + '/homepage.html');
+  res.sendFile("homepage.html");
 });
 
 
 // ====================== Launch Server ===========================
 
-app.listen(8000, () => {
-  console.log("Listening on port 8000...");
+app.listen(3000, () => {
+  console.log(`Server is running on http://localhost:3000`);
   // Maybe also get homepage / search screen?
 });

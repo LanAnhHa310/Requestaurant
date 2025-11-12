de7329d2e0367734eb50799560c438beb1b938ca
Jaxon <j.a.froderberg@proton.me>
2025-11-12 12:22:06 -0800

    feat: Adjusted merged fixes from feature/register-fix branch

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 // generate express application object.
 const express = require("express");
 
-var path = require('path'); // provides utilities for working with file and directory paths.
+// provides utilities for working with file and directory paths.
+var path = require('path'); 
 
 // Use user model exported from models directory ( connects to the db ).
 const User = require("./models/user");
 
 // ====================== SERVER SETUP ============================
 
+// Set application object equal to express obj.
 const app = express();
 
 // Parse JSON and form bodies
-app.use(express.json()); // Set application object equal to express obj.
+app.use(express.json());
 
-app.use(express.urlencoded({ extended: false })); // Used to tell the webserver to recognize incoming POST or PUT request data as string / array values.
+// Used to tell the webserver to recognize incoming POST or PUT request data as string / array values.
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname + '/public') ) );
 // app.use(express.static('public'));
@@ -47,39 +50,30 @@ app.get("/register", (req, res) => {
 
 app.post("/register", async (req,res) => {
 
-  // // Create new user DB entry from register webpage data:
-  // const newUser = new User({
-  //   userName: req.body.username,
-  //   password: req.body.password, // Needs to be hashed / secured later if possible!
-  //   email: req.body.email,
-  // });
-
-  // Add new user to the user database:
-  // try {
-  //   await newUser.save();
-  //   console.log(`Saved ${newUser.userName} to database successfully!`);
-  // } catch {
-  //   // Failed to register new user in DB:
-  //   res.status(400).send(ex.message);
-  // }
-
+  // Create new user DB entry from register webpage data and add to the database:
   try {
     const newUser = new User({
       userName: req.body.username,
-      password: req.body.password,
+      password: req.body.password, // Needs to be hashed / secured later if possible!
       email: req.body.email,
     });
     await newUser.save();
+
     console.log(`Saved ${newUser.userName} to database successfully!`, "→ collection:", newUser.collection.name);
-    return res.status(201).json({ message: "Registered", user: { username: newUser.userName, email: newUser.email } });
+
+    //return res.status(201).json({ message: "Registered", user: { username: newUser.userName, email: newUser.email } });
+
     } catch (err) {
+      // Failed to register new user in DB:
       return res.status(400).send(err.message);
     }
  
   // TOKEN: save information to identify which user is logged in.
-  // Use a sessionStorage username for now. Not best practice.
-  //console.log(`User ${newUser.userName} saved to sessionStorage`)
-  //sessionStorage.setItem("currentUser", newUser.userName);
+  
+  // Add token / user identification to response:
+
+  // Return final response:
+  return res.status(201).json({ message: "Registered", user: { username: newUser.userName, email: newUser.email } });
 });
 
 
@@ -91,37 +85,7 @@ app.use((req, res) => {
 
 // ====================== Helper methods: =========================
 
-// function validateRegistration( String username, String password, String  ) {
 
-//     let userName = document.getElementById("username").value;
-//     let userPassword = document.getElementById("password").value;
-//     let userPassConfirm = document.getElementById("passwordConfirm").value;
-
-//     // Check if password has the correct length:
-//     let passwordExp = RegExp(".{8}"); // <------ UPDATE: Set some more rules for this.
-//     if ( passwordExp.test( userPassword ) == false ) {
-//         alert("ERROR: Password is of insufficent length.");
-//         return false;
-//     }
-
-//     // Check if password entries match:
-//     if ( userPassword != userPassConfirm ) {
-//         alert("ERROR: Passwords do not match.");
-//         return false;
-//     }
-
-//     let userEmail = document.getElementById("email").value;
-
-//     // Check that email meets the required pattern:
-//     let re = /^(.+)@(.+)/; // <---- UPDATE: Make email validation more exact.
-//     let emailCheck = re.test( userEmail );
-//     if ( emailCheck == false ) {
-//         alert("ERROR: Email invalid.");
-//         return false;
-//     }
-
-//     return true;
-// }
 
 
 // ====================== Launch Server ===========================

e5d8163356ad948ab7c2a25e00398b721d31a0b1
Jaxon <j.a.froderberg@proton.me>
2025-12-03 21:29:06 -0800

    feat: Added sanitation to review pUT requests

--- server.js
+++ server.js
@@ -181,17 +181,6 @@ app.get("/api/reviews/user/:userName", async (req, res) => {
 // POST a new review
 app.post("/api/reviews", async (req, res) => {
   try {
-
-    // // Sanitize body before use:
-    // const cleanData = sanitize(req.body);
-    // const { username, password, email } = cleanData;
-
-    // //Check clean data types:
-    // if ( (typeof username !== "string") || (typeof password !== "string") || (typeof email !== "string") ) {
-    //   console.warn("Non-string registration data rejected", username);
-    //   return res.status(400).json({ error: "Non-string registration data rejected" });
-    // }
-
     const cleanData = sanitize(req.body);
     const {
       rating,
@@ -254,12 +243,29 @@ app.post("/api/reviews", async (req, res) => {
   }
 });
 
-
 // PUT update a review (user can only update their own reviews)
 app.put("/api/reviews/:reviewId", async (req, res) => {
   try {
-    const reviewId = req.params.reviewId;
-    const userName = req.body.userName; // User making the request
+
+    // // Sanitize body before use:
+    // const cleanData = sanitize(req.body);
+    // const { username, password, email } = cleanData;
+
+    // //Check clean data types:
+    // if ( (typeof username !== "string") || (typeof password !== "string") || (typeof email !== "string") ) {
+    //   console.warn("Non-string registration data rejected", username);
+    //   return res.status(400).json({ error: "Non-string registration data rejected" });
+    // }
+
+    const cleanData = sanitize(req.body);
+    const cleanParams = sanitize(req.params);
+    const { userName } = cleanData; // User making the request
+    const { reviewId } = cleanParams;
+
+    if ( (typeof reviewId !== "string") || (typeof userName !== "string") ) {
+      console.warn("Non-string review data rejected", userName);
+      return res.status(400).json({ error: "Non-string review data rejected" });
+    }
   
     // Find the review first to verify ownership
     const review = await Review.findById(reviewId);
